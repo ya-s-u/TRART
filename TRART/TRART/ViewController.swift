1849f5c6f321c6a027b91c839eb77b606ff39a89
Yohei <yoohei2323@gmail.com>
2015-09-11 13:43:45 +0900

    Xcodeファイルの追加

---
+++ TRART/TRART/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TRART
+//
+//  Created by Yohei Fusayasu on 9/11/15.
+//  Copyright (c) 2015 yohei2323. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TRART/TRARTTests/TRARTTests.swift
@@ -0,0 +1,36 @@
+//
+//  TRARTTests.swift
+//  TRARTTests
+//
+//  Created by Yohei Fusayasu on 9/11/15.
+//  Copyright (c) 2015 yohei2323. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TRARTTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
