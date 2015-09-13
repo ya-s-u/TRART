9fd0d9c8e4f9714d7b8dce151921f49a6cb04402
Yasuaki Goto <yasu1003@gmail.com>
2015-09-13 18:12:52 +0900

    [add] TrackTable view

--- TRART/TRART/MakeNewView.swift
+++ TRART/TRART/MakeNewView.swift
@@ -3,7 +3,6 @@ import UIKit
 class MakeNewView: UIView {
     
     override func awakeFromNib() {
-        
     }
     
 }
--- TRART/TRART/TracksTableCellView.swift
+++ TRART/TRART/TracksTableCellView.swift
@@ -1,21 +1,4 @@
-//
-//  TracksTableCellView.swift
-//  TRART
-//
-//  Created by Yohei Fusayasu on 9/13/15.
-//  Copyright (c) 2015 yohei2323. All rights reserved.
-//
-
 import UIKit
 
-class TracksTableCellView: UIView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+class TracksTableCellView: UITableViewCell {
 }
