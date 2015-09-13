184ca0151333eb5b9d83fe7fb8b6b30e8fd832e4
Yohei <yoohei2323@gmail.com>
2015-09-14 00:19:45 +0900

    [add] MainTableHeaderのxibを追加

---
+++ TRART/TRART/MainTableHeaderController.swift
@@ -0,0 +1,21 @@
+//
+//  MainTableHeaderController.swift
+//  TRART
+//
+//  Created by Yohei Fusayasu on 9/13/15.
+//  Copyright (c) 2015 yohei2323. All rights reserved.
+//
+
+import UIKit
+
+class MainTableHeaderController: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
