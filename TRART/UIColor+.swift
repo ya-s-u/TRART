2908b68b36b152f7c14730a0b99c70ff9fb3ae38
Yasuaki Goto <yasu1003@gmail.com>
2015-09-16 01:26:31 +0900

    [modify] UIColor.rgbColor method

--- TRART/UIColor+.swift
+++ TRART/UIColor+.swift
@@ -33,12 +33,12 @@ extension UIColor {
         return UIColor.redColor()
     }
     
-    class func rgbColor(rgbValue: UInt) -> UIColor {
+    class func rgbColor(rgbValue: UInt, alpha: Float = 1.0) -> UIColor {
         return UIColor(
             red:   CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
             green: CGFloat((rgbValue & 0x00FF00) >>  8) / 255.0,
             blue:  CGFloat( rgbValue & 0x0000FF)        / 255.0,
-            alpha: CGFloat(1.0)
+            alpha: CGFloat(alpha)
         )
     }
 }
