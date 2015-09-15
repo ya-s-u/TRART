e08c4fc9146d86b088876082baab08365b6cf52e
Yohei <yoohei2323@gmail.com>
2015-09-15 12:01:48 +0900

    [fix] directory-tmp

--- Pods/APIKit/APIKit/Request.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public protocol Request {
-    typealias Response: Any
-    
-    var URLRequest: NSURLRequest? { get }
-    
-    static func responseFromObject(object: AnyObject) -> Response?
-}
--- Pods/Target Support Files/KDEAudioPlayer/KDEAudioPlayer-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double KDEAudioPlayerVersionNumber;
-FOUNDATION_EXPORT const unsigned char KDEAudioPlayerVersionString[];
-
