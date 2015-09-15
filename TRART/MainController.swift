88426af7c04412a736ff8878cb047e6997f2bbef
Yohei <yoohei2323@gmail.com>
2015-09-16 00:20:50 +0900

    [add] Playlistのジャケットを表示

--- TRART/MainController.swift
+++ TRART/MainController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import RealmSwift
 
-class MainController: UIViewController {
+class MainController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    let realm = Realm()
+    var playlists: [Playlist] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +21,22 @@ class MainController: UIViewController {
         let header: MainTableHeaderController = UINib(nibName: "MainTableHeader", bundle: nil).instantiateWithOwner(self, options: nil)[0] as! MainTableHeaderController
         self.tableView.tableHeaderView = header
         
+        let realmResponse = realm.objects(Playlist)
+        for playlist in realmResponse {
+            playlists.append(playlist)
+        }
+        
+        println(self.playlists.count)
+        
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "segueToMakeNew", name: "CreateButton", object: nil)
+        //tableView Delegate
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +47,26 @@ class MainController: UIViewController {
         var storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         var makeNewViewController = storyboard.instantiateViewControllerWithIdentifier("MakeNew") as! UIViewController
         self.presentViewController(makeNewViewController, animated: true, completion: nil)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.playlists.count
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 300.0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("HomeTableViewCell") as! HomeTableViewCell
+        
+        cell.loadTemplate(playlists[indexPath.row])
+        
+        return cell
     }
     
     @IBAction func unwindToTop(segue: UIStoryboardSegue) {
