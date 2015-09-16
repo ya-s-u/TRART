d4bac8ae464b6b61a3db7c8ea09320f29b588fa0
Yasuaki Goto <yasu1003@gmail.com>
2015-09-16 11:27:21 +0900

    [add] waveform offset

--- TRART/PlayerView.swift
+++ TRART/PlayerView.swift
@@ -11,7 +11,7 @@ class PlayerView: UIView {
     @IBOutlet weak var mask: UIView!
     
     override func awakeFromNib() {
-        // register notification
+        // receive notification
         NSNotificationCenter.defaultCenter().addObserver(
             self,
             selector: "updatePlayingTracks:",
@@ -46,12 +46,12 @@ class PlayerView: UIView {
     func updatePlayingTracks(notification: NSNotification) {
         removeScrollViewSubViews()
         
-        var width = CGFloat(0.0)
+        var width = CGFloat(0)
         for (i, track) in enumerate(app.playingTracks) {
             let waveform = UIImage(named: "dummy-pulse")!
             let waveformView = UIImageView()
             waveformView.image = waveform
-            waveformView.frame = CGRectMake(waveform.size.width * CGFloat(i), 0, waveform.size.width, self.bounds.height)
+            waveformView.frame = CGRectMake(self.bounds.width/2 + waveform.size.width * CGFloat(i), 0, waveform.size.width, self.bounds.height)
             scrollView.addSubview(waveformView)
             
             width += waveform.size.width
