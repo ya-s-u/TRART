371034f48738cb86af5c58269c4094f52b62fdae
Yasuaki Goto <yasu1003@gmail.com>
2015-09-17 02:55:59 +0900

    [modify] collection view

--- TRART/SelectedView.swift
+++ TRART/SelectedView.swift
@@ -11,39 +11,31 @@ import UIKit
 class SelectedView: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    
     var app =  UIApplication.sharedApplication().delegate as! AppDelegate
     let playlistPlayer = PlaylistPlayerManager.sharedInstance
     
     override func awakeFromNib() {
-    // Cellに使われるクラスを登録.
-    collectionView.registerClass(SelectedTracksCell.self, forCellWithReuseIdentifier: "SelectedTracksCell")
-    
-    collectionView.delegate = self
-    collectionView.dataSource = self
+        collectionView.registerClass(SelectedTracksCell.self, forCellWithReuseIdentifier: "SelectedTracksCell")
+        collectionView.backgroundColor = UIColor.textViewColor()
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
-    
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 8
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        
         let cell: SelectedTracksCell = collectionView.dequeueReusableCellWithReuseIdentifier("SelectedTracksCell",
             forIndexPath: indexPath) as! SelectedTracksCell
         
-        cell.backgroundColor = UIColor.orangeColor()
+        cell.backgroundColor = UIColor.whiteColor()
         return cell
     }
     
-    /*
-    Cellが選択された際に呼び出される
-    */
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        
         println("Num: \(indexPath.row)")
-        
     }
-
 
 }
