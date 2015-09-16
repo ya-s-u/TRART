111adbeb25539dda3593610ce4c42aa7b5f78d6c
Yasuaki Goto <yasu1003@gmail.com>
2015-09-16 20:35:20 +0900

    [add] view

--- TRART/MakeNewNavigationController.swift
+++ TRART/MakeNewNavigationController.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 class MakeNewNavigationController: UINavigationController {
+    var selectedTracks: SelectedTracksView!
     var player: PlayerView!
     
     override func viewDidLoad() {
@@ -28,14 +29,19 @@ class MakeNewNavigationController: UINavigationController {
     }
     
     func showPlaylistPlayer(sender: AnyObject) {
+        selectedTracks = UINib(nibName: "SelectedTracks", bundle: nil).instantiateWithOwner(self, options: nil)[0] as! SelectedTracksView
+        selectedTracks.layer.frame = CGRectMake(0, 0, self.view.bounds.size.width, 80.0)
+        selectedTracks.layer.position = CGPoint(x: self.view.bounds.width/2, y: self.view.bounds.height - 110.0)
+        self.view.addSubview(selectedTracks)
+        
         player = UINib(nibName: "Player", bundle: nil).instantiateWithOwner(self, options: nil)[0] as! PlayerView
         player.layer.frame = CGRectMake(0, 0, self.view.bounds.size.width, 70.0)
         player.layer.position = CGPoint(x: self.view.bounds.width/2, y: self.view.bounds.height - 35.0)
-        
         self.view.addSubview(player)
     }
     
     func hidePlaylistPlayer(sender: AnyObject) {
+        selectedTracks.removeFromSuperview()
         player.removeFromSuperview()
     }
 }
